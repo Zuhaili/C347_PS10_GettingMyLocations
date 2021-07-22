eeee08d39b9cc4110c8d507a9b40b0f09cef2863
zuhaili <19004781@myrp.edu.sg>
2021-07-23 00:02:13 +0800

    PS10

--- app/src/main/java/sg/edu/rp/c346/id19004781/c347_ps10_gettingmylocations/MyService.java
+++ app/src/main/java/sg/edu/rp/c346/id19004781/c347_ps10_gettingmylocations/MyService.java
@@ -2,12 +2,17 @@ package sg.edu.rp.c346.id19004781.c347_ps10_gettingmylocations;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Binder;
+import android.os.Environment;
 import android.os.IBinder;
 import android.util.Log;
 
+import java.io.File;
+
 public class MyService extends Service {
 
+    private MediaPlayer player = new MediaPlayer();
     boolean started;
 
     @Override
@@ -17,21 +22,43 @@ public class MyService extends Service {
     }
 
     @Override
+    // execution of service will start on calling this method
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        try {
+            File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/PS10", "music.mp3");
+
+//            if (file.exists() == false) {
+//                boolean result = file.mkdir();
+//                if (result == true) {
+//                    Log.d("File Read/Write", "Folder created");
+//                    Toast.makeText(MyService.this, "Folder created", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+            // specify the path of the audio file
+            player.setDataSource(file.getPath());
+            player.prepare();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // providing the boolean value as true to play the audio on loop
+        player.setLooping(true);
+
+        // starting the process
+        player.start();
+
+        // returns the status of the program
+        return START_STICKY;
+    }
+
+    @Override
     public void onCreate() {
         Log.d("MyService", "Service created");
         super.onCreate();
     }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        if (started == false){
-            started = true;
-            Log.d("MyService", "Service started");
-        } else {
-            Log.d("MyService", "Service is still running");
-        }
-        return super.onStartCommand(intent, flags, startId);
-    }
+
 
     @Override
     public void onDestroy() {
